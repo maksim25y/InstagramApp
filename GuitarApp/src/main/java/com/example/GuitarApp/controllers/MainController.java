216acbd9d4b9b9f350a6b171a2511c4216fe6629
mudan <myudanov223@gmail.com>
2024-03-17 15:31:37 +0300

    Added controller for adding

--- GuitarApp/src/main/java/com/example/GuitarApp/controllers/MainController.java
+++ GuitarApp/src/main/java/com/example/GuitarApp/controllers/MainController.java
@@ -2,11 +2,16 @@ package com.example.GuitarApp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
     @GetMapping
     public String getMainPage(){
         return "index";
+    }
+    @PostMapping
+    public String addNewUserOnLesson(){
+        return "redirect:";
     }
 }
