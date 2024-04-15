f34b899e844195d0e4aaa553b2e47ae85edd4159
mudan <myudanov223@gmail.com>
2024-04-15 20:48:56 +0300

    added PosrService - adding, editing, deleting , gettina all posts, getting all posts by user, getting post by id

---
+++ demo/src/main/java/com/example/demo/dto/PostDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class PostDTO {
+    private Long id;
+    private String title;
+    private String caption;
+    private String location;
+    private Integer likes;
+    private Set<String> usersLiked;
+}
