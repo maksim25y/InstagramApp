e0ce551347a8d1b69e9f3d1966c92ea4ee3e3300
mudan <myudanov223@gmail.com>
2024-04-17 21:54:55 +0300

    added PostController

--- demo/src/main/java/com/example/demo/web/UserController.java
+++ demo/src/main/java/com/example/demo/web/UserController.java
@@ -38,7 +38,7 @@ public class UserController {
 
         return new ResponseEntity<>(userDTO, HttpStatus.OK);
     }
-
+    @PostMapping("/update")
     public ResponseEntity<Object>updateUser(@Valid @RequestBody UserDTO userDTO, BindingResult bindingResult,Principal principal){
         ResponseEntity<Object>errors = responseErrorValidation.mapValidationService(bindingResult);
         if(!ObjectUtils.isEmpty(errors))return errors;
