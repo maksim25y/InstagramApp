2afae74ef7eb16034ecbf977239b4eb88447069c
mudan <myudanov223@gmail.com>
2024-04-17 22:44:22 +0300

    added CommentService and CommentController

---
+++ demo/src/main/java/com/example/demo/web/CommentController.java
@@ -0,0 +1,62 @@
+package com.example.demo.web;
+
+import com.example.demo.dto.CommentDTO;
+import com.example.demo.entity.Comment;
+import com.example.demo.entity.Post;
+import com.example.demo.facade.CommentFacade;
+import com.example.demo.payload.response.MessageResponse;
+import com.example.demo.services.CommentService;
+import com.example.demo.validations.ResponseErrorValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/comment")
+@CrossOrigin
+public class CommentController {
+
+    @Autowired
+    private CommentFacade commentFacade;
+    @Autowired
+    private CommentService commentService;
+    @Autowired
+    private ResponseErrorValidation responseErrorValidation;
+    @PostMapping("/{postId}/create")
+    public ResponseEntity<Object>createComment(@Valid @RequestBody CommentDTO commentDTO,
+                                               BindingResult bindingResult,
+                                               @PathVariable("postId")String postId,
+                                               Principal principal){
+        ResponseEntity<Object>errors = responseErrorValidation.mapValidationService(bindingResult);
+        if(!ObjectUtils.isEmpty(errors))return errors;
+
+        Comment comment = commentService.saveComment(Long.parseLong(postId),commentDTO,principal);
+        CommentDTO createdComment = commentFacade.commentToCommentDTO(comment);
+
+        return new ResponseEntity<>(createdComment, HttpStatus.OK);
+    }
+
+    @GetMapping("/{postId}/all")
+    public ResponseEntity<List<CommentDTO>>getAllCommentsToPost(@PathVariable("postId")String postId){
+        List<CommentDTO>commentDTOList = commentService.getAllCommentsForPost(Long.parseLong(postId))
+                .stream()
+                .map(commentFacade::commentToCommentDTO)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(commentDTOList,HttpStatus.OK);
+    }
+    @PostMapping("/{commentId}/delete")
+    public ResponseEntity<MessageResponse>deleteComment(@PathVariable("commentId")String commentId){
+        commentService.deleteComment(Long.parseLong(commentId));
+        return new ResponseEntity<>(new MessageResponse("Comment was deleted"),HttpStatus.OK);
+    }
+}
--- demo/src/main/java/com/example/demo/web/PostController.java
+++ demo/src/main/java/com/example/demo/web/PostController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
